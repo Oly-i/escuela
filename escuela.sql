188fc99ed1b53c6b17a7a6d458e3c292b95bae92
Oly-i <al219630@alumnos.uacj.mx>
2025-02-20 09:51:13 -0700

    Update escuela.sql

--- escuela.sql
+++ escuela.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Feb 19, 2025 at 04:36 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 20-02-2025 a las 17:49:24
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,55 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `escuela`
+-- Base de datos: `escuela`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `carrera`
---
-
-CREATE TABLE `carrera` (
-  `Codigo` varchar(50) NOT NULL,
-  `Nombre` varchar(50) NOT NULL,
-  `Duracion` varchar(25) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-
---
--- Dumping data for table `carrera`
---
-
-INSERT INTO `carrera` (`Codigo`, `Nombre`, `Duracion`) VALUES
-('AR2514', 'Arquitectura', '4 años'),
-('DDMI93', 'Diseño Digital de Medios Interactivos', '4 años'),
-('DM0234', 'Diseño de Modas', '3 años'),
-('IAB350', 'Ingeniería Ambiental', '4 años');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `inscripcion`
---
-
-CREATE TABLE `inscripcion` (
-  `CodigoInscripcion` int(11) NOT NULL,
-  `Fecha` varchar(25) NOT NULL,
-  `CodigoAlumno` varchar(25) NOT NULL,
-  `CodigoCarrera` varchar(25) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-
---
--- Dumping data for table `inscripcion`
---
-
-INSERT INTO `inscripcion` (`CodigoInscripcion`, `Fecha`, `CodigoAlumno`, `CodigoCarrera`) VALUES
-(100, '2 feb 2025', '219630', 'AR2514');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `alumno`
+-- Estructura de tabla para la tabla `alumno`
 --
 
 CREATE TABLE `alumno` (
@@ -78,24 +36,76 @@ CREATE TABLE `alumno` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
--- Dumping data for table `alumno`
+-- Volcado de datos para la tabla `alumno`
 --
 
 INSERT INTO `alumno` (`Codigo`, `Nombre`, `Apellido`, `Edad`, `Telefono`) VALUES
+('193450', 'Alan', 'García Muñoz', 21, '6569984331'),
+('205634', 'Karla', 'Guerra Esquivel', 19, '6568741225'),
 ('219630', 'Sarah', 'Esparza', 20, '6563378337');
 
+-- --------------------------------------------------------
+
 --
--- Indexes for dumped tables
+-- Estructura de tabla para la tabla `carrera`
+--
+
+CREATE TABLE `carrera` (
+  `Codigo` varchar(50) NOT NULL,
+  `Nombre` varchar(50) NOT NULL,
+  `Duracion` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `carrera`
+--
+
+INSERT INTO `carrera` (`Codigo`, `Nombre`, `Duracion`) VALUES
+('AR2514', 'Arquitectura', '4 años'),
+('DDMI93', 'Diseño Digital de Medios Interactivos', '4 años'),
+('DM0234', 'Diseño de Modas', '3 años'),
+('IAB350', 'Ingeniería Ambiental', '4 años');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `inscripcion`
+--
+
+CREATE TABLE `inscripcion` (
+  `CodigoInscripcion` int(11) NOT NULL,
+  `Fecha` varchar(25) NOT NULL,
+  `CodigoAlumno` varchar(25) NOT NULL,
+  `CodigoCarrera` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `inscripcion`
+--
+
+INSERT INTO `inscripcion` (`CodigoInscripcion`, `Fecha`, `CodigoAlumno`, `CodigoCarrera`) VALUES
+(100, '2 de febrero de 2025', '219630', 'DM0234'),
+(101, ' 20 de julio de 2024', '205634', 'IAB350'),
+(102, '20 de diciembre de 2023', '193450', 'AR2514');
+
+--
+-- Índices para tablas volcadas
 --
 
 --
--- Indexes for table `carrera`
+-- Indices de la tabla `alumno`
+--
+ALTER TABLE `alumno`
+  ADD PRIMARY KEY (`Codigo`);
+
+--
+-- Indices de la tabla `carrera`
 --
 ALTER TABLE `carrera`
   ADD PRIMARY KEY (`Codigo`);
 
 --
--- Indexes for table `inscripcion`
+-- Indices de la tabla `inscripcion`
 --
 ALTER TABLE `inscripcion`
   ADD PRIMARY KEY (`CodigoInscripcion`),
@@ -103,27 +113,21 @@ ALTER TABLE `inscripcion`
   ADD KEY `FKCarrera` (`CodigoCarrera`);
 
 --
--- Indexes for table `alumno`
---
-ALTER TABLE `alumno`
-  ADD PRIMARY KEY (`Codigo`);
-
---
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT for table `inscripcion`
+-- AUTO_INCREMENT de la tabla `inscripcion`
 --
 ALTER TABLE `inscripcion`
   MODIFY `CodigoInscripcion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=123457;
 
 --
--- Constraints for dumped tables
+-- Restricciones para tablas volcadas
 --
 
 --
--- Constraints for table `inscripcion`
+-- Filtros para la tabla `inscripcion`
 --
 ALTER TABLE `inscripcion`
   ADD CONSTRAINT `inscripcion_ibfk_1` FOREIGN KEY (`CodigoAlumno`) REFERENCES `alumno` (`Codigo`) ON DELETE CASCADE ON UPDATE CASCADE,
